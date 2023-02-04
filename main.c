ca6b5e07466090596fbe13b7cb10551d26975540
MateusPitura <mateuspitura@gmail.com>
2023-02-04 17:33:36 -0300

    feat(c): escolher cartas
    Para poder escolher a carta criado sistema de posição, é escolhido duas cartas e apenas o conteúdo delas será exibido por 1 segundo. Criado 2 funções, uma para calcular a linha e a outra para a coluna da posição escolhida

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <time.h>
+#include <time.h> //Para o srand
+#include <windows.h> //Para o Sleep
 
 #define TAMANHO_TABULEIRO 4
 #define CODIGO_ASCII_A 65
@@ -12,12 +13,18 @@ int gerarSimboloAleatorio();
 int calcularQuantidadeSimbolos(int tamanhoMatriz);
 int gerarNumeroAleatorio(int menorNumeroAleatorio, int maiorNumeroAleatorio);
 int contarRepeticoesTabuleiro(int simboloProcurado);
-void exibirTabela();
+void escolherCartas();
+void exibirPosicoesCarta();
+void exibirCartaEscolhida(int primeiraCartaEscolhida, int segundaCartaEscolhida);
+int calcularLinhaEscolhida(int posicaoEscolhida);
+int calcularColunaEscolhida(int posicaoEscolhida);
 
 int main(){
     srand(time(NULL));
     inicializarTabuleiro();
-    exibirTabela();
+    do{
+        escolherCarta();
+    } while(1==1);
     return 0;
 }
 
@@ -62,11 +69,51 @@ int contarRepeticoesTabuleiro(int simboloProcurado){
     return count;
 }
 
-void exibirTabela(){
-    for(int i=0; i<TAMANHO_TABULEIRO; i++){
-        for(int j=0; j<TAMANHO_TABULEIRO; j++){
-            printf("%5c", tabuleiro[i][j]);
+void escolherCarta(){
+    int primeiraCartaEscolhida;
+    int segundaCartaEscolhida;
+    exibirPosicoesCarta();
+    printf("Escolha a primeira carta: ");
+    scanf("%d", &primeiraCartaEscolhida);
+    printf("Escolha a segunda carta: ");
+    scanf("%d", &segundaCartaEscolhida);
+    system("cls");
+    exibirCartaEscolhida(primeiraCartaEscolhida, segundaCartaEscolhida);
+    Sleep(1000);
+    system("cls");
+}
+
+void exibirPosicoesCarta(){
+    for(int i=1; i<=TAMANHO_TABULEIRO; i++){
+        for(int j=1; j<=TAMANHO_TABULEIRO; j++){
+            printf("%5d", (i*10)+j); //Suponha i=3, j=4. Irá exibir 34
         }
         printf("\n");
     }
+}
+
+void exibirCartaEscolhida(int primeiraCartaEscolhida, int segundaCartaEscolhida){
+    int linhaPrimeiraCartaEscolhida=calcularLinhaEscolhida(primeiraCartaEscolhida);
+    int colunaPrimeiraCartaEscolhida=calcularColunaEscolhida(primeiraCartaEscolhida);
+    int linhaSegundaCartaEscolhida=calcularLinhaEscolhida(segundaCartaEscolhida);
+    int colunaSegundaCartaEscolhida=calcularColunaEscolhida(segundaCartaEscolhida);
+    for(int i=1; i<=TAMANHO_TABULEIRO; i++){
+        for(int j=1; j<=TAMANHO_TABULEIRO; j++){
+            if((i==linhaPrimeiraCartaEscolhida && j==colunaPrimeiraCartaEscolhida) || (i==linhaSegundaCartaEscolhida && j==colunaSegundaCartaEscolhida)){
+                printf("%5c", tabuleiro[i-1][j-1]); //-1 pois o i e j deveriam começar no 0
+            }
+            else{
+                printf("%5d", (i*10)+j);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int calcularLinhaEscolhida(int posicaoEscolhida){
+    return posicaoEscolhida/10; //Mesmo que a divisão seja um número decimal, apenas a parte inteira retorna por ser int
+}
+
+int calcularColunaEscolhida(int posicaoEscolhida){
+    return posicaoEscolhida-((posicaoEscolhida/10)*10); //Ex.: 34/10=3 (po ser int a parte decimal é descartada). 3*10=30. 34-30=4
 }
