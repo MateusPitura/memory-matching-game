806e62972cd45ae99bd5247f574cd56e69bc1845
MateusPitura <mateuspitura@gmail.com>
2023-02-05 11:27:16 -0300

    feat(c): verifica cartas escolhidas
    Criada função que verifica cartas escolhidas, se elas são validas e caso sejam verifica se são iguais, em caso de acerto a carta será mostrada sempre 'virada'

--- main.c
+++ main.c
@@ -4,34 +4,38 @@
 
 #define TAMANHO_TABULEIRO 4
 #define CODIGO_ASCII_A 65
+#define POSICAO_MENOR_CARTA 11
 
-int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]={0}; //Inicializa tudo com zero
+int tabuleiroCartas[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]={0}; //Inicializa tudo com zero
+int tabuleiroPosicoes[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
 
-void inicializarTabuleiro();
-int gerarSimboloValido();
-int gerarSimboloAleatorio();
+void inicializarTabuleiroCartas(void);
+int gerarSimboloValido(void);
+int gerarSimboloAleatorio(void);
 int calcularQuantidadeSimbolos(int tamanhoMatriz);
 int gerarNumeroAleatorio(int menorNumeroAleatorio, int maiorNumeroAleatorio);
 int contarRepeticoesTabuleiro(int simboloProcurado);
-void escolherCartas();
-void exibirPosicoesCarta();
-void exibirCartaEscolhida(int primeiraCartaEscolhida, int segundaCartaEscolhida);
+void inicializarTabuleiroPosicoes(void);
+void escolherCarta(void);
+void exibirCartasTabuleiro(int primeiraCartaEscolhida, int segundaCartaEscolhida);
 int calcularLinhaEscolhida(int posicaoEscolhida);
 int calcularColunaEscolhida(int posicaoEscolhida);
+int verificarCartasEscolhidas(int primeiraCartaEscolhida, int segundaCartaEscolhida);
 
 int main(){
     srand(time(NULL));
-    inicializarTabuleiro();
+    inicializarTabuleiroCartas();
+    inicializarTabuleiroPosicoes();
     do{
         escolherCarta();
     } while(1==1);
     return 0;
 }
 
-void inicializarTabuleiro(){
+void inicializarTabuleiroCartas(){
     for(int i=0; i<TAMANHO_TABULEIRO; i++){
         for(int j=0; j<TAMANHO_TABULEIRO; j++){
-            tabuleiro[i][j]=gerarSimboloValido();
+            tabuleiroCartas[i][j]=gerarSimboloValido();
         }
     }
 }
@@ -61,7 +65,7 @@ int contarRepeticoesTabuleiro(int simboloProcurado){
     int count=0;
     for(int i=0; i<TAMANHO_TABULEIRO; i++){
         for(int j=0; j<TAMANHO_TABULEIRO; j++){
-            if(tabuleiro[i][j]==simboloProcurado){
+            if(tabuleiroCartas[i][j]==simboloProcurado){
                 count++;
             }
         }
@@ -69,51 +73,81 @@ int contarRepeticoesTabuleiro(int simboloProcurado){
     return count;
 }
 
+void inicializarTabuleiroPosicoes(){
+    for(int i=0; i<TAMANHO_TABULEIRO; i++){
+        for(int j=0; j<TAMANHO_TABULEIRO; j++){
+            tabuleiroPosicoes[i][j]=((i+1)*10)+(j+1);
+        }
+    }
+}
+
 void escolherCarta(){
     int primeiraCartaEscolhida;
     int segundaCartaEscolhida;
-    exibirPosicoesCarta();
-    printf("Escolha a primeira carta: ");
-    scanf("%d", &primeiraCartaEscolhida);
-    printf("Escolha a segunda carta: ");
-    scanf("%d", &segundaCartaEscolhida);
+    do{
+        Sleep(1000);
+        system("cls");
+        exibirCartasTabuleiro(0, 0);
+        printf("Escolha a primeira carta: ");
+        scanf("%d", &primeiraCartaEscolhida);
+        printf("Escolha a segunda carta: ");
+        scanf("%d", &segundaCartaEscolhida);
+    }while(verificarCartasEscolhidas(primeiraCartaEscolhida, segundaCartaEscolhida)==0);
     system("cls");
-    exibirCartaEscolhida(primeiraCartaEscolhida, segundaCartaEscolhida);
+    exibirCartasTabuleiro(primeiraCartaEscolhida, segundaCartaEscolhida);
     Sleep(1000);
-    system("cls");
 }
 
-void exibirPosicoesCarta(){
-    for(int i=1; i<=TAMANHO_TABULEIRO; i++){
-        for(int j=1; j<=TAMANHO_TABULEIRO; j++){
-            printf("%5d", (i*10)+j); //Suponha i=3, j=4. Irá exibir 34
+void exibirCartasTabuleiro(int primeiraCartaEscolhida, int segundaCartaEscolhida){
+    for(int i=0; i<TAMANHO_TABULEIRO; i++){
+        for(int j=0; j<TAMANHO_TABULEIRO; j++){
+            if(tabuleiroPosicoes[i][j]==primeiraCartaEscolhida || tabuleiroPosicoes[i][j]==segundaCartaEscolhida || tabuleiroPosicoes[i][j]==0){
+                printf("%5c", tabuleiroCartas[i][j]);
+            }
+            else{
+                printf("%5d", tabuleiroPosicoes[i][j]);
+            }
         }
         printf("\n");
     }
 }
 
-void exibirCartaEscolhida(int primeiraCartaEscolhida, int segundaCartaEscolhida){
+int verificarCartasEscolhidas(int primeiraCartaEscolhida, int segundaCartaEscolhida){
     int linhaPrimeiraCartaEscolhida=calcularLinhaEscolhida(primeiraCartaEscolhida);
     int colunaPrimeiraCartaEscolhida=calcularColunaEscolhida(primeiraCartaEscolhida);
     int linhaSegundaCartaEscolhida=calcularLinhaEscolhida(segundaCartaEscolhida);
     int colunaSegundaCartaEscolhida=calcularColunaEscolhida(segundaCartaEscolhida);
-    for(int i=1; i<=TAMANHO_TABULEIRO; i++){
-        for(int j=1; j<=TAMANHO_TABULEIRO; j++){
-            if((i==linhaPrimeiraCartaEscolhida && j==colunaPrimeiraCartaEscolhida) || (i==linhaSegundaCartaEscolhida && j==colunaSegundaCartaEscolhida)){
-                printf("%5c", tabuleiro[i-1][j-1]); //-1 pois o i e j deveriam começar no 0
-            }
-            else{
-                printf("%5d", (i*10)+j);
-            }
-        }
-        printf("\n");
+    if(primeiraCartaEscolhida==segundaCartaEscolhida){
+        printf("A mesma carta foi escolhida duas vezes\n");
+        system("pause");
+        return 0;
+    }
+    if(primeiraCartaEscolhida<POSICAO_MENOR_CARTA || segundaCartaEscolhida<POSICAO_MENOR_CARTA){
+        printf("Essa posicao nao existe\n");
+        system("pause");
+        return 0;
+    }
+    if(linhaPrimeiraCartaEscolhida>=TAMANHO_TABULEIRO || colunaPrimeiraCartaEscolhida>=TAMANHO_TABULEIRO || linhaSegundaCartaEscolhida>=TAMANHO_TABULEIRO || colunaSegundaCartaEscolhida>=TAMANHO_TABULEIRO){
+        printf("Essa posicao nao existe\n");
+        system("pause");
+        return 0;
+    }
+    if(tabuleiroPosicoes[linhaPrimeiraCartaEscolhida][colunaPrimeiraCartaEscolhida]==0 || tabuleiroPosicoes[linhaSegundaCartaEscolhida][colunaSegundaCartaEscolhida]==0){
+        printf("Essa posicao ja foi escolhida\n");        
+        system("pause");
+        return 0;
+    }
+    if(tabuleiroCartas[linhaPrimeiraCartaEscolhida][colunaPrimeiraCartaEscolhida]==tabuleiroCartas[linhaSegundaCartaEscolhida][colunaSegundaCartaEscolhida]){
+        tabuleiroPosicoes[linhaPrimeiraCartaEscolhida][colunaPrimeiraCartaEscolhida]=0;
+        tabuleiroPosicoes[linhaSegundaCartaEscolhida][colunaSegundaCartaEscolhida]=0;
+        return 1;
     }
 }
 
 int calcularLinhaEscolhida(int posicaoEscolhida){
-    return posicaoEscolhida/10; //Mesmo que a divisão seja um número decimal, apenas a parte inteira retorna por ser int
+    return (posicaoEscolhida/10)-1; //Mesmo que a divisão seja um número decimal, apenas a parte inteira retorna por ser int. -1 pois a matriz começa em 0
 }
 
 int calcularColunaEscolhida(int posicaoEscolhida){
-    return posicaoEscolhida-((posicaoEscolhida/10)*10); //Ex.: 34/10=3 (po ser int a parte decimal é descartada). 3*10=30. 34-30=4
+    return (posicaoEscolhida-((posicaoEscolhida/10)*10))-1; //Ex.: 34/10=3 (po ser int a parte decimal é descartada). 3*10=30. 34-30=4. -1 pois a matriz começa em 0
 }
